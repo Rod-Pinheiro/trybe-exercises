26c2675dd78fc89fd847023a0b7416c98fcac02b
rodPinheiro <rod_bma@hotmail.com>
2021-06-24 17:37:21 -0300

    atualiza 008.js - avaliador de numero par

--- exercises/javaScript/008.js
+++ exercises/javaScript/008.js
@@ -1,9 +1,10 @@
+/* funcao que avalia se PELO MENOS um numero eh par */
 function avaliaPar() {
   let primeiro = Math.ceil(Math.random() * 10)
   let segundo = Math.ceil(Math.random() * 10)
   let terceiro = Math.ceil(Math.random() * 10)
 
-  if (primeiro % 2 === 0 && segundo % 2 === 0 && terceiro % 2 === 0) {
+  if (primeiro % 2 === 0 || segundo % 2 === 0 || terceiro % 2 === 0) {
     return true;
   } else {
     return false;
