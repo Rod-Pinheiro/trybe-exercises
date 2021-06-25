4102e73e8dbdb3fe93300e9c88bbdaa815e90d5b
rodPinheiro <rod_bma@hotmail.com>
2021-06-25 19:07:41 -0300

    adiciona exercicio 4

--- exercises/javaScript/logicaEAlgoriimos/003.js
+++ exercises/javaScript/logicaEAlgoriimos/003.js
@@ -1,9 +1,7 @@
 function asteriscosTrianguloInvertido(n) {
   let space = " ";
   let ast = "";
-  
   for (let j = 0; j < n; j++) {
-     
     ast = ast + "*"
     console.log(space.repeat(n - j) + ast);
   }
