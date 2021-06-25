71b1589e843016938e88a82682eaef617ea23b80
rodPinheiro <rod_bma@hotmail.com>
2021-06-25 15:30:00 -0300

    adiciona exercicio 2

---
+++ exercises/javaScript/logicaEAlgoriimos/002.js
@@ -0,0 +1,11 @@
+function asteriscosTriangulo (n , m){
+  for (let i = 0; i < n; i++) {
+    let ast = "";
+    
+    for (let j = 0; j < m; j++) {
+       
+      ast = ast + "*"
+      console.log(ast);
+    }
+  }
+} 
